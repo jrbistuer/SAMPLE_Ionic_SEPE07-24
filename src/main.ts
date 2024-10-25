c57bf2fe86b9888860a1de249f317da0092f063b
jrbistuer <jrbistuer@gmail.com>
2024-10-25 16:15:14 +0200

    angular fire

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,19 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() => initializeApp({"projectId":"del-cifo02","appId":"1:32213368281:web:0b186ed5d05e3bee02adc8","storageBucket":"del-cifo02.appspot.com","apiKey":"AIzaSyAB80RDDAH6-9BphAVpYEvLaYK_kkr23UE","authDomain":"del-cifo02.firebaseapp.com","messagingSenderId":"32213368281","measurementId":"G-C3ZPJVG0FW"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
 });

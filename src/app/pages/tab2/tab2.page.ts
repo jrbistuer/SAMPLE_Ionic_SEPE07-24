5140582a5b7b024e9009377b5d6974267c5bfc55
jrbistuer <jrbistuer@gmail.com>
2024-11-08 17:00:38 +0100

    stats

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Toast } from '@capacitor/toast';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
 })
 export class Tab2Page {
 
   constructor() {}
+
+  async showToast() {
+      await Toast.show({
+        text: 'Hello!',
+        duration: 'long',
+        position: 'bottom'
+      });
+  }
 
 }
